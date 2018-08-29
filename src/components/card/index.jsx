cf78522e2293368b533d1fe8e00b065db669a71a
Eric Murta <ericmurta@hotmail.com>
2018-08-28 23:30:02 -0300

    Add notifications

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -4,11 +4,21 @@ import './style.css';
 
 export const Card: SComponent<Props> = ({
   description,
+  amountNotifications = 1,
 }) => (
   <div className="Card">
     <div className="Card-description">
       {description}
     </div> 
+
+    <div className="Card-footer">
+      <div className="Card-footer-notifications">
+        <span className="Card-footer-icon far fa-bell" />
+        <span className="Card-footer-amount">
+          {amountNotifications}
+        </span>
+      </div>
+    </div>
   </div>  
 );
 
--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import List from './components/list';
-import Card from './components/card';
+import List from '../components/list';
+import Card from '../components/card';
+
+import './style.css';
 
 const Home = () => (
-  <div>
+  <div className="Home">
     <List title="Reproved">
       <Card description="Tempo 2:40 - Ao visualizar documento e clicar em informações duas vezes, ele não fecha a info, ele abre novamente." />
       <Card description="Tempo 2:40 - Ao visualizar documento e clicar em informações duas vezes, ele não fecha a info, ele abre novamente." />
